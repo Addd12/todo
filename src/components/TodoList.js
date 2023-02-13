bbfe3048638caf1427d7321bdb3a2d75508e6b12
Addd12 <addd.github@gmail.com>
2023-02-13 10:54:58 +0100

    edit todo

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -14,6 +14,19 @@ function TodoList() {
         //console.log(...todos) this line prints the list on the console of the browser
     }
 
+    const updateTodo = (todoId, newValue) => {
+        if(!newValue.text || /^\s*$/.test(newValue.text)){
+          return
+      }
+      setTodos(prev => prev.map(item => (item.id === todoId ? newValue : item)))
+    }
+
+    const removeTodo = id =>{
+      const removeArr = [...todos].filter(todo => todo.id !== id)
+
+      setTodos(removeArr)
+    }
+
     const completeTodo = id => {
       let updatedTodos = todos.map(todo=>{
         if (todo.id === id){
@@ -27,7 +40,7 @@ function TodoList() {
     <div>
       <h1>what's the plan for today?</h1>
       <TodoForm onSubmit={addTodo}/>
-      <Todo todos={todos} completeTodo={completeTodo}/>
+      <Todo todos={todos} completeTodo={completeTodo} removeTodo={removeTodo} updateTodo={updateTodo}/>
     </div>
   )
 }
